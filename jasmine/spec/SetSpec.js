06cd02d98d5c9a4b8da4ee97e93e40ebd40f9f5a
Tobi Lehman <tobi.lehman@gmail.com>
2014-12-06 18:08:06 -0800

    add set member, each, subset and equals methods

--- jasmine/spec/SetSpec.js
+++ jasmine/spec/SetSpec.js
@@ -3,5 +3,45 @@ describe("Set", function() {
     var A = new Set([2,3]);
     expect(A.cardinality()).toBe(2);
   });
+
+  it("checks for membership", function() {
+    var A = new Set(['to', 'be']);
+    expect(A.member('be')).toBe(true);
+    expect(A.member('not to be')).toBe(false);
+  });
+
+  it("has a subset method that behaves well", function() {
+    var O = new Set;
+    var A = new Set([1,2]);
+    var B = new Set([1,2,3]);
+
+    // empty set is subset of itself and all sets
+    expect(O.subset(O)).toBe(true);
+    expect(O.subset(A)).toBe(true);
+    
+    // any set is a subset of itself
+    expect(A.subset(A)).toBe(true);
+    // A is a subset of B
+    expect(A.subset(B)).toBe(true);
+    // B is not a subset of A
+    expect(B.subset(A)).toBe(false);
+  });
+
+  it("has an equals method that behaves well", function() {
+    var A = new Set([1,2,3]);
+    var B = new Set([2,1,3]);
+    var C = new Set([1,1,2,3]);
+
+    // symmetry of equivalence
+    expect(A.equals(B)).toBe(true);
+    expect(B.equals(A)).toBe(true);
+
+    expect(B.equals(C)).toBe(true);
+    expect(C.equals(B)).toBe(true);
+
+
+    // transitivity of equivalence
+    expect(A.equals(C)).toBe(true);
+  });
 });
 
--- jasmine/src/Set.js
+++ jasmine/src/Set.js
@@ -2,7 +2,38 @@ var Set = function(elements) {
   var elements = elements || [];
 
   return {
-    cardinality: function() { return elements.length; }
+    cardinality: function() { return elements.length; },
+    member: function(element) {
+      var isMem = false;
+      for(var i = 0; i < elements.length; i++) {
+        if(elements[i] === element) {
+          isMem = true;
+        }
+      }
+      return isMem;
+    },
+    equals: function(other) {
+      return this.subset(other) && other.subset(this);
+    },
+    each: function(fn) {
+      for(var i = 0; i < elements.length; i++) {
+        fn(elements[i]);
+      }
+    },
+    /* if all elements of A are in B, then A is a subset of B */
+    subset: function(other) {
+      var A = this;
+      var B = other;
+
+      var allAareInB = true;
+      A.each(function(element) {
+        if(!B.member(element)) {
+          allAareInB = false;
+        }
+      });
+
+      return allAareInB;
+    }
   }
 }
 
