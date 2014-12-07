c9069c0edf90c5eba6bfcb7a7df2539603d5bb81
Tobi Lehman <tobi.lehman@gmail.com>
2014-12-06 17:34:02 -0800

    pull ComponentMap into its own file and add spec

---
+++ jasmine/spec/ComponentMapSpec.js
@@ -0,0 +1,7 @@
+describe("ComponentMap", function() {
+  it("counts the number of connected components in a grid", function() {
+    var connComp = new ComponentMap([], 0);
+    expect(connComp.numberComponents).toBe(0);
+  });
+});
+
