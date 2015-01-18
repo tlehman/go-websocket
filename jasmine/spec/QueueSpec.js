1513a7987dc23a089507b46a8abb9391b88f2a58
Tobi Lehman <tobi.lehman@gmail.com>
2015-01-18 09:36:20 -0800

    add queue toString method

--- jasmine/spec/QueueSpec.js
+++ jasmine/spec/QueueSpec.js
@@ -5,5 +5,21 @@ describe("Queue", function() {
     q.enqueue(1);
     expect(q.empty()).toBe(false);
   });
+
+  it("has a toString method", function() {
+    var p = new Queue();
+    var q = new Queue();
+    
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    p.enqueue(new Pair(1,2));
+    p.enqueue(new Pair(0,4));
+    p.enqueue(new Pair(3,3));
+
+    expect(p.toString()).toBe("[(1,2),(0,4),(3,3)]");
+    expect(q.toString()).toBe("[1,2,3]");
+  });
 });
 
--- jasmine/src/Queue.js
+++ jasmine/src/Queue.js
@@ -9,6 +9,24 @@ var Queue = function() {
     },
     empty: function() {
       return _elements.length == 0;
+    },
+    toString: function() {
+      var str = "[";
+      var elem;
+
+      for(var i = 0; i < _elements.length; i++) {
+        elem = _elements[i];
+        if(typeof(elem) === "object") {
+          str += elem.toString();
+        } else {
+          str += elem;
+        }
+        if(i < _elements.length-1) {
+          str += ",";
+        }
+      }
+
+      return str + "]";
     }
   };
 }
