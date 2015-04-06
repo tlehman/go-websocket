2a18926196850182fa9e9c5d049d2d76c761edc9
Tobi Lehman <tobi.lehman@gmail.com>
2015-04-05 20:43:44 -0700

    add main.go (backend)

--- main.go
+++ main.go
@@ -4,10 +4,29 @@ package main
    so happens that I wrote the back end in Go(lang)
 
    This code organizes the data of all the interacting players.
+
+   There are three main parts to this program:
+
+	 (1) The part that interacts with the database, this is the
+	     central repository of all game data, users, matches,
+	     and game states.
+		 This part also includes a PostgreSQL schema that the Go
+		 code assumes.
+
+	 (2) Game logic, even though there is game logic in go_game.js,
+	     there are some safeguards in this back end so that evil
+		 game players can't cheat.
+
+	 (3) The part that interacts with the user, this receives
+	     messages from a websocket, parses it, and decides where
+		 to delegate it.
 */
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 )
 
 type User struct {
@@ -33,6 +52,29 @@ type State struct {
 }
 
 func main() {
-	u := User{id: 1, name: "tlehman"}
-	fmt.Printf("%v\n", u)
+	reader := bufio.NewReader(os.Stdin)
+	var line string
+	var err error
+
+	for {
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		fmt.Printf(line)
+	}
+}
+
+func loadUsersFromDb() []User {
+	return make([]User, 10)
+}
+
+func loadMatcesFromDb() []Match {
+	return make([]Match, 10)
+}
+
+func loadStatesFromDb() []State {
+	return make([]State, 10)
 }
